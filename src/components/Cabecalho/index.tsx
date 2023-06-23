93c7a2ed6ac5f344a6f25e5efb2f8564cae708a3
CwM03 <120037381+CwM03@users.noreply.github.com>
2023-06-23 10:41:58 -0300

    Add files via upload

---
+++ src/components/Cabecalho/index.tsx
@@ -0,0 +1,9 @@
+import { HeaderCabecalho } from './styles'
+
+const Cabecalho = () => (
+  <HeaderCabecalho>
+    <h1>EBAC Jobs</h1>
+  </HeaderCabecalho>
+)
+
+export default Cabecalho
---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,17 @@
+import { Container } from '../../../src/styles'
+
+import { Formulario, FormDiv, HeroTitle } from './styles'
+
+const Hero = () => (
+  <Formulario>
+    <Container>
+      <FormDiv>
+        <HeroTitle>
+          As melhores vagas para tecnologia, design e artes visuais.
+        </HeroTitle>
+      </FormDiv>
+    </Container>
+  </Formulario>
+)
+
+export default Hero
